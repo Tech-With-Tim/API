e92f84cbec5b2cc61297f5f1801d4ca467b38caa
SylteA <sylteandreas7@gmail.com>
2020-11-16 13:47:53 +0100

    Added endpoint to CREATE users

--- utils/request.py
+++ utils/request.py
@@ -1,15 +1,10 @@
 from quart import Request as BaseRequest
 
-from typing import Optional
-
-
-from db.models import User
-
 
 class Request(BaseRequest):
     """Custom request class to implement the user kwarg."""
 
     @property
-    def user(self) -> Optional[User]:
+    def user(self):
         """Getting the user is handled by our `UserMiddleware`"""
         return self.scope.get("user", None)
