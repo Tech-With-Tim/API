be8594160197a0f18beded418227d77ffc7f8bd0
SylteA <sylteandreas7@gmail.com>
2020-12-10 14:31:30 +0100

    Update launch.py

--- launch.py
+++ launch.py
@@ -22,16 +22,26 @@ env = {
 loop = asyncio.get_event_loop()
 
 
+try:
+    with open(".env", "r") as f:
+        env_file = {
+            key: arg
+            for (key, arg) in [line.strip().split("=") for line in f.readlines()]
+        }
+except FileNotFoundError:
+    env_file = {}
+
+
 for key in env.keys():
     try:
         env[key] = os.environ[key]
     except KeyError:
-        sys.stderr.write(f"Found no `{key}` var in env, exiting..."), exit(1)
+        try:
+            env[key] = env_file[key]
+        except KeyError:
+            sys.stderr.write(f"Found no `{key}` var in env, exiting..."), exit(1)
 
 app.config.from_mapping(mapping=env)
-
-# TODO: Handle environment values in docker.
-#       https://docs.docker.com/compose/environment-variables/
 
 
 def run_async(func: Coroutine) -> Any:
@@ -39,16 +49,12 @@ def run_async(func: Coroutine) -> Any:
     return loop.run_until_complete(func)
 
 
-async def setup_db(quart_app: Quart) -> None:
+async def setup_db() -> None:
     log = logging.getLogger("DB")
 
     log.debug("Attempting to initialize database connection.")
 
-    await Model.create_pool(
-        uri=env["DB_URI"],
-        max_con=10,
-        loop=loop
-    )
+    await Model.create_pool(uri=env["DB_URI"], max_con=10, loop=loop)
 
     log.debug("Connected to database `{}`".format(env["DB_URI"].split("/")[-1]))
 
@@ -63,7 +69,7 @@ async def setup_session(quart_app: Quart) -> ClientSession:
     return quart_app.session
 
 
-run_async(setup_db(quart_app=app))
+run_async(setup_db())
 run_async(setup_session(quart_app=app))
 
 
@@ -83,11 +89,16 @@ def initdb():
 def dropdb():
     async def drop_db():
         from postDB import Model
+
         for model in Model.all_models():
             print(await model.drop_table(verbose=True))
             click.echo(f"Dropping table {model.__name__}")
 
     run_async(drop_db())
+
+
+# TODO: Add CLI command to create APP user
+# TODO: Add CLI command to add User to admin group.  (After we make groups)
 
 
 @app.cli.command()
