78fa708d420efe1d3e74bde0e881504a5467c08e
SylteA <sylteandreas7@gmail.com>
2020-12-16 16:21:24 +0100

    commit to work on other pc.

--- api/blueprints/guilds/views/guilds.py
+++ api/blueprints/guilds/views/guilds.py
@@ -1,5 +1,6 @@
 from quart import Response, request, jsonify
 from logging import getLogger
+from typing import Optional
 
 from .. import blueprint
 from api.models import Guild
@@ -7,6 +8,7 @@ import utils
 
 request: utils.Request
 logger = getLogger("/logs")
+NoneType = type(None)
 
 
 @blueprint.route("/<int:guild_id>", methods=["GET"])
@@ -15,14 +17,42 @@ async def fetch_guild(guild_id: int):
     """Fetch `Guild` instance by log ID."""
     guild = await Guild.fetch(id=guild_id)
     if guild is None:
-        return jsonify({"error": "404 Not Found - No Guild with that id in database."}), 404
+        return jsonify(dict(
+            error="404 Not Found",
+            message="No Guild with that id in database."
+        )), 404
 
     return jsonify(guild.as_dict())
 
 
 @blueprint.route('/', methods=["POST"])
-@utils.auth_required  # TODO -> app_only
-@utils.expects_data()
+@utils.app_only
+@utils.expects_data(
+    id=int,
+    name=str,
+    region=str,
+    owner_id=int,
+    icon_hash=(NoneType, str),
+    muted_role_id=(NoneType, Optional[int]),
+    log_channel_id=(NoneType, Optional[int]),
+    verification_channel_id=(NoneType, Optional[int]),
+)
 async def create_guild(data: dict):
     """Create a new guild."""
-    pass
+    guild = Guild(**data)
+
+    if not await guild.create():
+        return jsonify(dict(
+            error="400 Bad Request",
+            message="Guild already exists."
+        )), 400
+
+    return jsonify(dict(
+        message="Guild created."
+    )), 201
+
+
+@blueprint.route('/<int:guild_id>', methods=["PUT"])
+async def update_guild(guild_id: int):
+    """Update guild with provided kwargs."""
+
