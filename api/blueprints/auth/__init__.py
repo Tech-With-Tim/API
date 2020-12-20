b06527fa0407c3370baa289620751bd33d2d693e
SylteA <sylteandreas7@gmail.com>
2020-12-20 23:35:36 +0100

    Add another comment to describe what we're doing...

--- api/blueprints/auth/__init__.py
+++ api/blueprints/auth/__init__.py
@@ -7,6 +7,7 @@ blueprint: Blueprint = Blueprint("auth", __name__)
 
 def setup(app: Quart, url_prefix: str):
     from .views import placeholder
+    # Import anything in the file so we load the blueprint routes.
 
     # Reguster blueprint to Quart instance.
     app.register_blueprint(blueprint=blueprint, url_prefix=url_prefix)
--- api/blueprints/guilds/__init__.py
+++ api/blueprints/guilds/__init__.py
@@ -5,6 +5,7 @@ blueprint: Blueprint = Blueprint("guilds", __name__)
 
 def setup(app: Quart, url_prefix: str):
     from .views import placeholder
+    # Import anything in the file so we load the blueprint routes.
 
     # Reguster blueprint to Quart instance.
     app.register_blueprint(blueprint=blueprint, url_prefix=url_prefix)
