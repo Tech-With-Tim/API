b046afd24ec588a44c1090e2200675649aec742f
SylteA <sylteandreas7@gmail.com>
2021-01-20 10:28:13 +0100

    Fix black fail.

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,10 @@ from .middleware import TokenAuthMiddleware
 from .time import snowflake_time
 from .request import Request
 
-__all__ = ("TokenAuthMiddleware", "snowflake_time", "Request", "expects_data", "expects_files")
+__all__ = (
+    "TokenAuthMiddleware",
+    "snowflake_time",
+    "Request",
+    "expects_data",
+    "expects_files",
+)
