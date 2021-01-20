c9ff2c248eb385991c09ba201997605cc7f46945
SylteA <sylteandreas7@gmail.com>
2021-01-20 10:22:00 +0100

    Added validators for request JSON/FORM data and files by filename.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
+from .validators import expects_data, expects_files
 from .middleware import TokenAuthMiddleware
 from .time import snowflake_time
 from .request import Request
 
-__all__ = ("TokenAuthMiddleware", "snowflake_time", "Request")
+__all__ = ("TokenAuthMiddleware", "snowflake_time", "Request", "expects_data", "expects_files")
