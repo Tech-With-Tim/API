52d13b6057cd772ab1d1926a68a448caba6e0b6a
sarzz2 <sarthaksingh2001@gmail.com>
2021-04-10 15:12:00 +0530

    added tests

--- tests/test_weekly_challenges.py
+++ tests/test_weekly_challenges.py
@@ -80,7 +80,7 @@ async def test_create_weekly_challenge(
     auth_app: QuartClient, db, data: dict, status_code: int
 ):
     response = await auth_app.post(
-        "/", json=data, headers={"authorization": auth_app.token}
+        "/wkc", json=data, headers={"authorization": auth_app.token}
     )
     assert response.content_type == "application/json"
     assert response.status_code == status_code
