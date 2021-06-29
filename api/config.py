de15be713f917b28cd6ce6e3670f95d535120997
M7MD <55202038+mohamed040406@users.noreply.github.com>
2021-06-29 12:31:57 +0200

    Update config

--- api/config.py
+++ api/config.py
@@ -12,34 +12,19 @@ DISCORD_CLIENT_SECRET: typing.Optional[str]
 
 
 def load_env(path: str):
-    try:
-        with open(path) as f:
-            env_file = {
-                key.strip(): arg.strip()
-                for (key, arg) in [
-                    line.strip().split("=") for line in f.readlines() if line.strip()
-                ]
-            }
-    except FileNotFoundError:
-        env_file = {}
-
-    for key, val in env_file.items():
-        if not os.environ.get(key, None):
-            os.environ[key] = env_file.get(key, None)
-
-    for k, v in __annotations__.copy().items():
-        if typing.get_origin(v) is None:
-            if not (val := os.environ.get(k, None)):
-                log.error(f"Required environment variable {k!r} is missing")
+    for config, annotation in __annotations__.copy().items():
+        if typing.get_origin(annotation) is None:
+            if not (val := os.environ.get(config, None)):
+                log.error(f"Required environment variable {config!r} is missing")
                 raise EnvironmentError(
-                    f"Required environment variable {k!r} is missing"
+                    f"Required environment variable {config!r} is missing"
                 )
 
-            globals()[k] = val
-            log.debug(f"Loaded {k!r} from environment variables")
-        elif typing.get_origin(v) is typing.Union and None in typing.get_args(v):
-            if not (val := os.environ.get(k, None)):
-                log.warning(f"Optional environment variable {k!r} is missing")
+            globals()[config] = val
+            log.debug(f"Loaded {config!r} from environment variables")
+        elif typing.get_origin(annotation) is typing.Union and None in typing.get_args(annotation):
+            if not (val := os.environ.get(config, None)):
+                log.warning(f"Optional environment variable {config!r} is missing")
 
-            globals()[k] = val
-            log.debug(f"Loaded {k!r} from environment variables")
+            globals()[config] = val
+            log.debug(f"Loaded {config!r} from environment variables")
