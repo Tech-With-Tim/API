8aa0a9d6be415a59a9a009f5485dbb300d1dff41
mohamed040406 <mohamed040406@users.noreply.github.com>
2021-09-05 20:50:49 +0200

    update docstring

--- utils/permissions.py
+++ utils/permissions.py
@@ -5,7 +5,7 @@ from api.models.permissions import BasePermission, Administrator
 def has_permissions(
     permissions: int, required: List[Union[int, BasePermission]]
 ) -> bool:
-    """Returns `True` if this role has all provided permissions"""
+    """Returns `True` if `permissions` has all required permissions"""
     if permissions & Administrator().value:
         return True
 
@@ -20,7 +20,7 @@ def has_permissions(
 
 
 def has_permission(permissions: int, permission: Union[BasePermission, int]) -> bool:
-    """Returns `True` if this role has the provided permission"""
+    """Returns `True` if `permissions` has required permission"""
     if permissions & Administrator().value:
         return True
 
